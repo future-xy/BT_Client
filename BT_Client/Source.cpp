5092b5be69c76a63fd2823cea8e0a7aa4c72fceb
y f <fy38607203@163.com>
2019-06-12 15:29:34 +0800

    V2.0 基本完成客户端框架

--- BT_Client/Source.cpp
+++ BT_Client/Source.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 #include<fstream>
+#include<sstream>
 
 #include<string>
 #include<vector>
 #include<map>
+#include<algorithm>
 
 #include<thread>
 #include<mutex>
 
+#define NOMINMAX 
 #include<WinSock2.h>
 #include<stdlib.h>
 
@@ -23,11 +26,15 @@ using namespace std;
 const int bufflen = 20000;
 const string server_port = "10086";
 const string lis_port = "50520";
+const int piece_length = 20000;
 
+int t_num = 0;
 mutex mu;
 
 int download(torrent_file);
+int download_t(string ip, string port, string filename, int start, int num, int index);
 int upload();
+int upload_t(SOCKET sock);
 
 int main()
 {
@@ -59,7 +66,7 @@ int main()
 			cout << "输入服务器地址：";
 			cin >> server;
 
-			string t_name = make_torrent(filename, server);
+			string t_name = make_torrent(filename, piece_length, server);
 			//向服务器上传消息
 			sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 			memset(&sin, 0, sizeof(sin));
@@ -75,7 +82,9 @@ int main()
 				cerr << "ERROR!\n";
 				continue;
 			}
-
+			send(sock, "0", 1, 0);
+			send(sock, filename.c_str(), filename.size(), 0);
+			send(sock, lis_port.c_str(), lis_port.size(), 0);
 
 			cout << t_name << "创建成功！" << endl;
 		}
@@ -92,7 +101,7 @@ int main()
 			//告诉服务器离开了
 			if (isconnected)
 			{
-				send(sock, "FIN", 3, 0);
+				send(sock, "2", 1, 0);
 			}
 			break;
 		}
@@ -104,8 +113,8 @@ int main()
 
 int download(torrent_file tf)
 {
-	struct sockaddr_in sin, fsin;
-	SOCKET sock, fsock;
+	struct sockaddr_in sin;
+	SOCKET sock;
 
 	char buff[bufflen];
 
@@ -119,63 +128,105 @@ int download(torrent_file tf)
 
 	if (ret == 0)
 	{
+		send(sock, "1", 1, 0);
 		send(sock, tf.name.c_str(), tf.name.length(), 0);
-		recv(sock, buff, bufflen, 0);
+		int cc = recv(sock, buff, bufflen, 0);
+		buff[cc] = 0;
 		map<string, string> target;
 		//解析返回的文件拥有者列表
-
-		if (target.size())
+		istringstream ifs(buff);
+		int n;
+		ifs >> n;
+		for (int i = 0; i < n; i++)
 		{
-			//选择一个拥有者进行连接
-			auto item = target.begin();
-			memset(&fsin, 0, sizeof(fsin));
-			sin.sin_family = AF_INET;
-			sin.sin_addr.s_addr = inet_addr(item->first.c_str());
-			sin.sin_port = htons((u_short)stoi(item->second));
+			string ip, port;
+			ifs >> ip >> port;
+			target[ip] = port;
+		}
+		//计算文件的总块数
+		int piece_num = (tf.length + tf.piece_length - 1) / tf.piece_length;
+		//计算每个线程应该下载的块数
+		int task_num = (piece_num + n - 1) / n;
+		//一个线程向一个拥有者索要
+		int index = 0;
+		for (auto item : target)
+		{
+			/*最后一个可能会有不足*/
+			thread temp(download_t, item.first, item.second, tf.name, index*task_num*tf.piece_length,
+				min(task_num, piece_num)*tf.piece_length, index);
+			++index;
+			piece_num -= task_num;
+			//访问临界区
+			mu.lock();
+			t_num++;
+			mu.unlock();
+			temp.detach();
+		}
+		//让权等待所有线程下载完毕
+		while (t_num)
+			std::this_thread::yield();
 
-			ret = connect(fsock, (struct sockaddr*)&fsin, sizeof(fsin));
-			//传输数据
-			send(fsock, tf.name.c_str(), tf.name.length(), 0);
-			//结束符
-			string endstr = tf.name + "!!EndFile@@";
-			int endlen = endstr.length();
-
-			ofstream myof(tf.name, ios::binary);
-			memset(buff, 0, sizeof(buff));
-			while (true)
+		ofstream myofs(tf.name, ios::binary);
+		char tempbuff[bufflen];
+		for (int i = 0; i < n; i++)
+		{
+			ifstream tempifs(tf.name + to_string(i) + ".temp", ios::binary);
+			while (!tempifs.eof())
 			{
-				int cc = recv(fsock, buff, sizeof(buff), 0);
-				//如果收到的数据长度恰好为结束符长度，则判断是否为结束符
-				if (cc == endlen)
-				{
-					buff[cc] = 0;
-					if (string(buff) == endstr)
-						break;
-				}
-				if (cc > 0)
-					myof.write(buff, cc);
-				else
-					break;
-				memset(buff, 0, sizeof(buff));
+				tempifs.read(tempbuff, bufflen);
+				myofs.write(tempbuff, tempifs.gcount());
 			}
-			myof.close();
-
-			//	send(sock, "FIN", 3, 0);
+			tempifs.close();
 		}
-		else
-		{
-			cerr << "No !" << endl;
-		}
+		myofs.close();
+		//通知服务器
+		send(sock, "0", 1, 0);
+		send(sock, tf.name.c_str(), tf.name.size(), 0);
+		send(sock, lis_port.c_str(), lis_port.size(), 0);
 	}
 
 	return 0;
+}
+
+int download_t(string ip, string port, string filename, int start, int num, int index)
+{
+	ofstream myofs(filename + to_string(index) + ".temp", ios::binary);
+
+	char buff[bufflen];
+
+	SOCKET sock;
+	struct sockaddr_in sin;
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = inet_addr(ip.c_str());
+	sin.sin_port = htons((u_short)stoi(port));
+	connect(sock, (struct sockaddr*)&sin, sizeof(sin));
+	//发送文件名
+	send(sock, filename.c_str(), filename.size(), 0);
+	//发送起始位置
+	string temp = to_string(start);
+	send(sock, temp.c_str(), temp.size(), 0);
+	//发送字节数
+	temp = to_string(num);
+	send(sock, temp.c_str(), temp.size(), 0);
+
+	while (num > 0)
+	{
+		int cc = recv(sock, buff, bufflen, 0);
+		myofs.write(buff, cc);
+		num -= cc;
+	}
+	myofs.close();
+
+	return num;
 }
 
 int upload()
 {
 	SOCKET msock, sock;
 	struct sockaddr_in sin, fsin;
-	int alen;
+	int alen = sizeof(struct sockaddr_in);
 
 	msock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -190,26 +241,38 @@ int upload()
 	while (true)
 	{
 		sock = accept(msock, (struct sockaddr *)&fsin, &alen);
-		char buff[bufflen];
-		int n = recv(sock, buff, bufflen, 0);
-		if (n > 0)
-			buff[n] = 0;
-		else
-		{
-
-		}
-		string filename(buff);
-		ifstream myifs(filename, ios::binary);
-		while (!myifs.eof())
-		{
-			memset(buff, 0, sizeof(buff));
-			myifs.read(buff, bufflen);
-			send(sock, buff, myifs.gcount(), 0);
-		}
-		string endstr = filename + "!!EndFile@@";
-		send(sock, endstr.c_str(), endstr.length(), 0);
-		myifs.close();
+		thread temp(upload_t, sock);
+		//
+		temp.detach();
 	}
 
 	return 0;
+}
+
+int upload_t(SOCKET sock)
+{
+	char buff[bufflen];
+	//文件名
+	int cc = recv(sock, buff, bufflen, 0);
+	buff[cc] = 0;
+	ifstream myifs(string(buff), ios::binary);
+	//起始位置
+	cc = recv(sock, buff, bufflen, 0);
+	buff[cc] = 0;
+	int start = stoi(string(buff));
+	//字节数
+	cc = recv(sock, buff, bufflen, 0);
+	buff[cc] = 0;
+	int num = stoi(string(buff));
+	//移动文件指针
+	myifs.seekg(start, ios::beg);
+	while (!myifs.eof() && num > 0)
+	{
+		myifs.read(buff, min(bufflen, num));
+		send(sock, buff, myifs.gcount(), 0);
+		num -= myifs.gcount();
+	}
+	myifs.close();
+
+	return num;
 }
